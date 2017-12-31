79dbe3a4a6b96e20a3b536c3342d159b58cd9bd4
adnanhanafi <34978749+adnanhanafi@users.noreply.github.com>
2017-12-31 16:41:54 +0800

    Created hello-world.cs

---
+++ hello-world.cs
@@ -0,0 +1,10 @@
+// This is just a simple "Hello World!" program for testing
+class HelloWorld
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World!");
+    Console.Write("Press any key to close...");
+    Console.ReadKey();
+  }
+}
